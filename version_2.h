853f5b2a413f9fee172f910533b923e99952657e
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-05-23 21:01:51 +0930

    Update version_2.h

--- version_2.h
+++ version_2.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_2_H
 #define VERSION_2_H
 
-int extract_serverini_file();
+void extract_serverini_file();
 void comment_server_section();
 void directories_structure();
 void icrc_pattern_identification();
